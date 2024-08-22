ad04d62a7dc736464340753661b06450dca23fda
Vera-new <56616618+Vera-new@users.noreply.github.com>
2024-08-22 20:50:06 +0700

    load task 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -50,7 +50,7 @@ public class Main {
 
 // Задача 3
 //Перевернуть строку не используя reverse().
-
+/*
 import java.util.Scanner;
 
 public class Main {
@@ -59,11 +59,27 @@ public class Main {
         System.out.print("Введите строку: ");
         String str = scanner.nextLine();
         StringBuilder builder = new StringBuilder();
-        String reversedStr = "";
         for (int i = str.length() - 1; i >= 0; i--) {
             builder.append(str.charAt(i));
         }
         System.out.println("Перевернутая строка: " + builder);
     }
-}
+}*/
 
+//Задача 4.
+// Вводится строка, состоящая из слов, разделенных пробелами. Необходимо вывести в консоль первые буквы каждого слова.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String str = scanner.nextLine();
+        String[] words = str.split(" ");
+        for (String word : words) {
+            System.out.print(word.charAt(0));
+        }
+        System.out.println();
+    }
+}
